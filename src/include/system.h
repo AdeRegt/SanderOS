1c19dff09d8f254e3afd6d6578224acf013db329
AdeRegt <AdeRegt@github.com>
2018-02-10 12:51:11 +0100

    printf

--- src/include/system.h
+++ src/include/system.h
@@ -3,3 +3,5 @@ void kernel_main();
 // screen
 void printf(const char* format,...);
 void putc(const char a);
+char * itoa( int value, char * str, int base );
+void puts(const char* format);
--- src/screen.c
+++ src/screen.c
@@ -11,6 +11,51 @@ char backgroundcolor = 0x07;
 int curX = 0;
 int curY = 0;
 
+void puts(const char* format){
+	int xcount = 0;
+	char deze;
+	while((deze = format[xcount++])!='\0'){
+		putc(deze);
+	}
+}
+
+char * itoa( int value, char * str, int base ){
+    char * rc;
+    char * ptr;
+    char * low;
+    // Check for supported base.
+    if ( base < 2 || base > 36 )
+    {
+        *str = '\0';
+        return str;
+    }
+    rc = ptr = str;
+    // Set '-' for negative decimals.
+    if ( value < 0 && base == 10 )
+    {
+        *ptr++ = '-';
+    }
+    // Remember where the numbers start.
+    low = ptr;
+    // The actual conversion.
+    do
+    {
+        // Modulo is negative for negative value. This trick makes abs() unnecessary.
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz"[35 + value % base];
+        value /= base;
+    } while ( value );
+    // Terminating the string.
+    *ptr-- = '\0';
+    // Invert the numbers.
+    while ( low < ptr )
+    {
+        char tmp = *low;
+        *low++ = *ptr;
+        *ptr-- = tmp;
+    }
+    return rc;
+}
+
 void putc(const char a){
 	if(a=='\n'){
 		curY++;
@@ -42,6 +87,11 @@ void printf(const char* format,...){
 			if(deze=='c'){
 				char c = (char) va_arg(parameters, int /* char promotes to int */);
 				putc(c);
+			}else if(deze=='i'){
+				long c = (long) va_arg(parameters, long);
+				char* x = "       ";
+				itoa(c,x,10);
+				puts(x);
 			}
 		}else{
 			putc(deze);
