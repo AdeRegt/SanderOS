abee8f2dca7e20b8d44e959f4042737922d776ac
AdeRegt <AdeRegt@github.com>
2018-02-11 00:00:36 +0100

    auto

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,27 @@
 #include <system.h>
 
 void kernel_main(){
-	printf("
-	\n
-	\n
-	");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("   S A N D E R S L A N D O  O P E R A T I N G   S Y S T E M   \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
 }
--- src/kernel.c~
+++ src/kernel.c~
@@ -1,5 +1,8 @@
 #include <system.h>
 
 void kernel_main(){
-	printf("Hello, world!\nAls het goed is,zie je nu een 'C' : %c\nAls het goed is zie je nu een 15 : %i\nAls het goed is zie je nu een 0xABC: %x\nen \[41mnu is het rood",'C',15,0xABC);
+	printf("
+	\n
+	\n
+	");
 }
