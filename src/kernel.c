04ddb88517d0088c0ca0f4658c6eb06e0d8f42bd
AdeRegt <AdeRegt@github.com>
2018-02-11 00:11:07 +0100

    auto

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,16 @@
 #include <system.h>
 
 void kernel_main(){
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
+	printf("\n                                                                       ");
+	printf("\n           _/ _/ _/                                                    ");
+	printf("\n        _/                                                             ");
+	printf("\n          _/                                                           ");
+	printf("\n             _/                                                        ");
+	printf("\n                _/                                                     ");
+	printf("\n                _/                                                     ");
+	printf("\n             _/                                                        ");
+	printf("\n      _/ _/                                                            ");
+	printf("\n                                                                       ");
 	printf("             S A N D E R S L A N D O  O P E R A T I N G   S Y S T E M   \n");
 	printf("             An 32 bit operatingsystem created by Alexandros de Regt\n");
 	printf("             Loading services ...\n");
