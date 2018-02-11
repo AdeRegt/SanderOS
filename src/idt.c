a4f76767c67027346c0727a7b7e8e364da95f5d5
AdeRegt <AdeRegt@github.com>
2018-02-11 10:15:23 +0100

    auto

--- src/idt.c
+++ src/idt.c
@@ -1,3 +1,14 @@
 #include <system.h>
 
-void setupIDT(){}
+void setupIDT(){
+	
+}
+
+static inline void lidt(void* base, unsigned short size){   // This function works in 32 and 64bit mode
+    struct {
+        unsigned short length;
+        void*    base;
+    } __attribute__((packed)) IDTR = { size, base };
+ 
+    asm ( "lidt %0" : : "m"(IDTR) );  // let the compiler choose an addressing mode
+}
---
+++ src/ports.c
@@ -0,0 +1,49 @@
+void outportb(unsigned short port, unsigned char val){
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+    /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+unsigned char inportb(unsigned short port){
+    unsigned char ret;
+    asm volatile ( "inb %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
+void outportw(unsigned short port, unsigned short val){
+    asm volatile ( "outw %0, %1" : : "a"(val), "Nd"(port) );
+    /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+unsigned short inportw(unsigned short port){
+    unsigned short ret;
+    asm volatile ( "inw %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
+void outportl(unsigned short port, unsigned long val){
+    asm volatile ( "outl %0, %1" : : "a"(val), "Nd"(port) );
+    /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+unsigned long inportl(unsigned short port){
+    unsigned long ret;
+    asm volatile ( "inl %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
+
