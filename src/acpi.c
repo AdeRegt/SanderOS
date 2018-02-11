7f5c015dc27cbcc21d4c430322ca374a0423c95b
AdeRegt <AdeRegt@github.com>
2018-02-11 18:16:33 +0100

    auto

--- src/acpi.c
+++ src/acpi.c
@@ -3,7 +3,7 @@
 int memcmp(const void* aptr, const void* bptr, int size) {
 	const unsigned char* a = (const unsigned char*) aptr;
 	const unsigned char* b = (const unsigned char*) bptr;
-	for (size_t i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++) {
 		if (a[i] < b[i])
 			return -1;
 		else if (b[i] < a[i])
