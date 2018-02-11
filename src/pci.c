0d5631a87f3f68f0ce0dcbe342a1ea61da387d1f
AdeRegt <AdeRegt@github.com>
2018-02-11 12:08:08 +0100

    auto

--- src/pci.c
+++ src/pci.c
@@ -1,2 +1,20 @@
 #include <system.h>
 
+unsigned short pciConfigReadWord (unsigned char bus, unsigned char slot, unsigned char func, unsigned char offset){
+    unsigned long address;
+    unsigned long lbus  = (unsigned long)bus;
+    unsigned long lslot = (unsigned long)slot;
+    unsigned long lfunc = (unsigned long)func;
+    unsigned short tmp = 0;
+ 
+    /* create configuration address as per Figure 1 */
+    address = (unsigned long)((lbus << 16) | (lslot << 11) |
+              (lfunc << 8) | (offset & 0xfc) | ((uint32_t)0x80000000));
+ 
+    /* write out the address */
+    outportl (0xCF8, address);
+    /* read in the data */
+    /* (offset & 2) * 8) = 0 will choose the first word of the 32 bits register */
+    tmp = (unsigned short)((inportl (0xCFC) >> ((offset & 2) * 8)) & 0xffff);
+    return (tmp);
+ }
