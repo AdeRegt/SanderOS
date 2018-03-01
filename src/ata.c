8c36b2edbbbcbd562bdbfbdc32c14c083381298e
AdeRegt <AdeRegt@github.com>
2018-03-01 21:32:55 +0100

    auto

--- src/ata.c
+++ src/ata.c
@@ -1,14 +1,16 @@
 #include <system.h>
 
 void loadHDDSector(ata_device dev,char* location,long LBA,long count){
-	outportb(0x1F6, 0xE0 | (dev.slave << 4) | ((LBA >> 24) & 0x0F));
-	outportb(0x1F1, 0x00);
-	outportb(0x1F2, (unsigned char) count);
-	outportb(0x1F3, (unsigned char) LBA);
-	outportb(0x1F4, (unsigned char)(LBA >> 8));
-	outportb(0x1F5, (unsigned char)(LBA >> 16));
-	outportb(0x1F7, 0x20);
-	insw(0x1F0, location,count*512);
+	outportb(dev.io_base+6, 0xE0 | (dev.slave << 4) | ((LBA >> 24) & 0x0F));
+	outportb(dev.io_base+1, 0x00);
+	outportb(dev.io_base+2, (unsigned char) count);
+	outportb(dev.io_base+3, (unsigned char) LBA);
+	outportb(dev.io_base+4, (unsigned char)(LBA >> 8));
+	outportb(dev.io_base+5, (unsigned char)(LBA >> 16));
+	outportb(dev.io_base+7, 0x20);
+	int status;
+	while((status = inportb(dev.io_base+7)) & 0x80 ){}
+	insw(dev.io_base+0, location,count*512);
 }
 
 void detectHDDFilesystems(ata_device dev){
