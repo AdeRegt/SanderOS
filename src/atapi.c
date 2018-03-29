d919733c2f83f239765893705fa96a4f3641a3fd
AdeRegt <AdeRegt@github.com>
2018-03-29 20:54:24 +0200

    auto

--- src/atapi.c
+++ src/atapi.c
@@ -72,7 +72,7 @@ unsigned char* readCDROM(char* path){
 			}else{
 				cls();
 				i = 0;
-				for(i = 0 ; i < 100 ; i++){
+				for(i = 0 ; i < 200 ; i++){
 					printf("%c",buffer[i]);
 				}
 				goto exception;
