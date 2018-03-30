95644a8e7666c0c5782ca71bcac30f5a684699ca
AdeRegt <AdeRegt@github.com>
2018-03-30 21:35:18 +0200

    auto

--- src/kernel.c
+++ src/kernel.c
@@ -28,7 +28,7 @@ void kernel_main(){
 	}
 	setupIDT();
 	detectDevices();
-	unsigned char* msx = readCDROM("/BOOT/GRUB");
+	unsigned char* msx = readCDROM("/BOOT/GRUB/GRUB.CFG");
 	printf(">>>>>>>>>>>>>>%s",msx);
 	initialiseKeyboard();
 	//acpiEnable();
