d8857cd56ddfeecf6ee744f6a732c105a0562536
AdeRegt <AdeRegt@github.com>
2018-04-17 23:10:40 +0200

    auto

--- src/atapi.c
+++ src/atapi.c
@@ -101,7 +101,7 @@ unsigned char* readCDROM(char* path){
 			}
 			unsigned int tgy = i-ftf-1;
 			if(buffer[i-ftf-1]==(ftf+2)){
-				unsigned char txt = buffer[tgy-30];
+				unsigned char txt = buffer[tgy-29];
 				printf("YAY at %x \n",txt);
 				readRawCDROM(txt,1,(unsigned char*)buffer);
 				return (unsigned char*)buffer;
