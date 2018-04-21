8c3379c56c50be31c992c62f53b9c2e637bcb6b5
AdeRegt <AdeRegt@github.com>
2018-04-21 19:34:18 +0200

    auto

--- src/atapi.c
+++ src/atapi.c
@@ -99,7 +99,7 @@ unsigned char* readCDROM(char* path){
 					goto skipcontext;
 				}
 			}
-			unsigned int tgy = (i-ftf-1)-30;
+			unsigned int tgy = (i-ftf-1)-29;
 			printf("==> %x ",getB(tgy));
 			if(buffer[i-ftf-1]==(ftf+2)){
 				
