ec4b191a1b713f2757b9080c4c32bf85bcd0fe44
AdeRegt <AdeRegt@github.com>
2018-04-24 22:14:16 +0200

    auto

--- src/mouse.c
+++ src/mouse.c
@@ -1,13 +1,22 @@
 #include <system.h>
 void mouselib_int_wait_0();
+void mouselib_int_wait_1();
 
 void setup_mouse(){
-	mouselib_int_wait_0();
+	mouselib_int_wait_1();
 }
 
 void mouselib_int_wait_0(){
 	while(1){
-		if(inportb(0x64)&1){
+		if(inportb(0x64)&0){
+			break;
+		}
+	}
+}
+
+void mouselib_int_wait_1(){
+	while(1){
+		if(!(inportb(0x64)&1)){
 			break;
 		}
 	}
