6151617eabbd0e098799a697457d29c354a6a7f3
AdeRegt <AdeRegt@github.com>
2018-04-24 22:22:46 +0200

    auto

--- src/mouse.c
+++ src/mouse.c
@@ -1,27 +1,36 @@
 #include <system.h>
-void mouselib_int_wait_0();
-void mouselib_int_wait_1();
+inline void mouse_wait(char a_type);
 
 void setup_mouse(){
-	mouselib_int_wait_1();
+	mouse_wait(1);
 	outportb(0x64,0xA8);
-	mouselib_int_wait_1();
-	outportb(0x64,0xA8);
-	mouselib_int_wait_0();
+	mouse_wait(1);
+	outportb(0x64,0x20);
+	mouse_wait(0);
 }
 
-void mouselib_int_wait_0(){
-	while(1){
-		if((inportb(0x64)&1)==1){
-			break;
-		}
-	}
-}
-
-void mouselib_int_wait_1(){
-	while(1){
-		if(!(inportb(0x64)&2)){
-			break;
-		}
-	}
+inline void mouse_wait(char a_type){
+  short _time_out=100000; 
+  if(a_type==0)
+  {
+    while(_time_out--) //Data
+    {
+      if((inportb(0x64) & 1)==1)
+      {
+        return;
+      }
+    }
+    return;
+  }
+  else
+  {
+    while(_time_out--) //Signal
+    {
+      if((inportb(0x64) & 2)==0)
+      {
+        return;
+      }
+    }
+    return;
+  }
 }
