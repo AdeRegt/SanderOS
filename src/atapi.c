02d79852c9cee74177d1c18c9b8a9026b8c6e79e
AdeRegt <AdeRegt@github.com>
2018-04-25 20:34:09 +0200

    auto

--- src/atapi.c
+++ src/atapi.c
@@ -142,7 +142,7 @@ unsigned char* readCDROM(char* path){
 				q++;
 				t++;
 			}
-			for(unsigned int r = 0 ; r < q-1 ; r++){
+			for(unsigned int r = 0 ; r < q ; r++){
 				unsigned char A = buffer[(i-q)+r];
 				pathlist[pointer++] = A;
 			}
