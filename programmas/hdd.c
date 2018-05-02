c666d7305c399640be1f200fdbc333b66e1c18d9
AdeRegt <AdeRegt@github.com>
2018-05-02 21:06:51 +0200

    auto

--- programmas/hdd.c
+++ programmas/hdd.c
@@ -1,5 +1,5 @@
 #include <system.h>
 
-void main(ata_device *base){
-	printf("Hello small world at IOBase %x!",base.io_base);
+void main(ata_device *dev){
+	printf("Hello small world at IOBase %x!",dev.io_base);
 }
--- programmas/hdd.c~
+++ programmas/hdd.c~
@@ -1,5 +1,5 @@
 #include <system.h>
 
-void main(ata_device base){
+void main(ata_device *base){
 	printf("Hello small world at IOBase %x!",base.io_base);
 }
