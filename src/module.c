7029515878cc6e558760cc41f44bc2a83a75e86b
AdeRegt <AdeRegt@github.com>
2018-05-07 19:49:24 +0200

    auto

--- src/module.c
+++ src/module.c
@@ -20,3 +20,32 @@ void insmod(unsigned char* path,void* arguments){
 		}
 	}
 }
+
+void* loadExecutable(unsigned char* path){
+	unsigned char* msx = readCDROM(path);
+	if(strcmp(msx,(unsigned char*)"ERROR")==0){
+		printf("\nUNABLE TO LOAD %s !!\n",path);
+		return NULL;
+	}else{
+		return elf_load_file(msx);
+	}
+}
+
+int exec(unsigned char* path,void* arguments){
+	printf("--EXEC--\n");
+	printf("  -> loading file\n");
+	unsigned char* msx = readCDROM(path);
+	if(strcmp(msx,(unsigned char*)"ERROR")==0){
+		printf(" -> Unable to locate executable\n");
+	}else{
+		printf("  -> parsing file\n");
+		void* location = elf_load_file(msx);
+		if(location==NULL){
+			printf("\n  -> exec returned error!\n");
+		}else{
+			printf("  -> calling file at %x \n",location);
+			int (*foo)(void*) = (void*)location;
+			return foo(arguments);
+		}
+	}
+}
