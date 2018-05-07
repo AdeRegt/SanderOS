ecb18d6eec74f5b318c7ad664cd2704fae5d5694
AdeRegt <AdeRegt@github.com>
2018-05-07 19:22:08 +0200

    auto

--- src/keyboard.c
+++ src/keyboard.c
@@ -106,7 +106,7 @@ void keyboard_int(){
 		if(karakter>=0xb0&&karakter<0xbe){
 			unsigned long valve = karakter - 0xb0;
 			printf("\nF%x press detected! Time to yield!!\n",valve+1);
-			//yield();
+			yield();
 		}else if(karakter==0xbf){
 			shiftisin = 0x00;
 		}else{ 
