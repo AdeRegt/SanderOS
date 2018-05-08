6e6a994d6b321b3fa617215c852e3024ad6762a7
AdeRegt <AdeRegt@github.com>
2018-05-08 18:56:51 +0200

    auto

--- src/keyboard.c
+++ src/keyboard.c
@@ -112,7 +112,7 @@ void keyboard_int(){
 			shiftisin = 0x00;
 		}else{ 
 			//printf("%c",karakter);
-			((unsigned volatile char*)bufferconstant)[0] = (unsigned char)karakter;
+			((unsigned volatile char*)bufferconstant)[0] = (unsigned volatile char)karakter;
 			//printf("%c",((unsigned char*)0x10000)[0]);
 		}
 	}else if(deze == 42 || deze == 54){
