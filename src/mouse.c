db3ba22b1abaa4ad963d1af2f2adfdaf89ffb1a9
AdeRegt <AdeRegt@github.com>
2018-05-11 20:20:48 +0200

    auto

--- src/mouse.c
+++ src/mouse.c
@@ -8,12 +8,41 @@ void mouselib_int_wait_1(){
 		}
 	}
 }
+
+
+mouselib_int_wait_0:
+	mov cx, 65000
+	mov dx, 0x64
+.wait:
+	in al, dx
+	bt ax, 0
+	jc .okay
+	loop .wait
+.okay:
+	ret
+
+void mouselib_int_wait_0(){
+	while(1){
+		unsigned char dx = inportb(0x64);
+		if((dx & 0)>0){
+			break;
+		}
+	}
+}
 	
 void mouse_init(){
 	printf(">>STARTING<<");
 	asm volatile("cli");
 	mouselib_int_wait_1();
 	outportb(0x64,0xA8);
+	mouselib_int_wait_1();
+	outportb(0x64,0x20);
+	mouselib_int_wait_0();
+	unsigned char x = inportb(0x60) | 0x02;
+	mouselib_int_wait_1();
+	outportb(0x64,0x60);
+	mouselib_int_wait_1();
+	outportb(0x60,x);
 	asm volatile("sti");
 	printf(">>READY<<");
 }
