f951da1c0c837c868b3d35371411dfdab2be787d
AdeRegt <AdeRegt@github.com>
2018-05-11 20:07:18 +0200

    auto

--- src/mouse.c
+++ src/mouse.c
@@ -1,3 +1,16 @@
 #include <system.h>
 
-void mouse_init(){}
+void mouselib_int_wait_1(){
+	while(1){
+		unsigned char dx = inportb(0x64);
+		if((dx & 1)==1){}
+	}
+}
+	
+void mouse_init(){
+	asm volatile("cli");
+	mouselib_int_wait_1();
+	outport(0x64,0xA8);
+	asm volatile("sti");
+	printf(">>READY<<");
+}
