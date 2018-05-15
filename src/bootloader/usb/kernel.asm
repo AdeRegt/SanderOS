f46d8f52d767b530538b6bb22841aa437b150152
AdeRegt <AdeRegt@github.com>
2018-05-16 00:23:12 +0200

    auto

--- src/bootloader/usb/kernel.asm
+++ src/bootloader/usb/kernel.asm
@@ -16,7 +16,28 @@ os_main:
 	mov fs, ax			; live entirely in 64K
 	mov gs, ax
 
-	cmp dl, 0
+	mov byte [bootdevice],dl
 	
+	mov si,welcomemessage
+	call print_string
 	cli
 	hlt
+	
+bootdevice db 0x00
+welcomemessage db "Tot nutoe lijkt alles gewoon goed te werken!",0x00
+
+print_string:				; Output string in SI to screen
+	pusha
+
+	mov ah, 0Eh			; int 10h teletype function
+
+.repeat:
+	lodsb				; Get char from string
+	cmp al, 0
+	je .done			; If char is zero, end of string
+	int 10h				; Otherwise, print it
+	jmp short .repeat
+
+.done:
+	popa
+	ret
